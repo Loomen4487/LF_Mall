7475cea26f2fb977f02c1ee08ec57251c0fd9859
park-sanguk <tkddnrtmxk@naver.com>
2024-09-23 14:44:51 +0900

    base

---
+++ finalProject/src/main/java/com/example/finalProject/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.finalProject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security)throws Exception{
+        security.csrf(cs->cs.disable())
+                .formLogin(login->login.loginProcessingUrl("/login").defaultSuccessUrl("/"))
+                .authorizeHttpRequests(ah->ah.requestMatchers("/user/**").authenticated()
+                        .anyRequest().permitAll());
+        return security.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
