2d8499f9c0ecd486d53612fef176e85ee377ed22
bando327 <bando327@gmail.com>
2024-09-24 10:02:08 +0900

    0924 1000

---
+++ finalProject/src/main/resources/static/js/main.js
@@ -0,0 +1,25 @@
+<!-- Swiper JS -->
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  slidesPerGroup: 4,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
