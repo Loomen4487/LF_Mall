4823ec6047704cecc5ae32b08fdb596f0c8941f9
bando327 <bando327@gmail.com>
2024-09-25 14:58:13 +0900

    0925 1500

--- finalProject/src/main/resources/static/js/main.js
+++ finalProject/src/main/resources/static/js/main.js
@@ -1,48 +1,33 @@
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+/>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 <!-- Swiper JS -->
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-  slidesPerGroup: 4,
+<script>
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+    slidesPerGroup: 4,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-
-const swiper = new Swiper('.swiper-item', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-  slidesPerGroup: 4,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination-item',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next-item',
-    prevEl: '.swiper-button-prev-item',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar-item',
-  },
-});
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+</script>
