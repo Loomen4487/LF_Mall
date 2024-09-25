e4885555dc3d29972eca449457d52046ee321c02
park-sanguk <tkddnrtmxk@naver.com>
2024-09-25 11:27:08 +0900

    마이페이지

--- finalProject/src/main/resources/static/js/detailItem.js
+++ finalProject/src/main/resources/static/js/detailItem.js
@@ -9,3 +9,15 @@ minus.addEventListener("click",function(){
     if(count>1)count--;
     document.querySelector("#count").value=count;
 })
+const info = document.querySelectorAll(".ProductDetail_tabList__G8yFd>li");
+const infoUl = document.querySelector(".info-list");
+let infoIdx=0;
+info.forEach((item,index)=>{
+    item.addEventListener("click",function(){
+        infoUl.children[infoIdx].classList.remove("selected");
+        infoUl.children[infoIdx].children[1].style.display="none";
+        this.classList.add("selected");
+        this.children[1].style.display="block";
+        infoIdx = index;
+    })
+})
