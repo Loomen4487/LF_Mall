d2eb7eceb1be1c8e864338c5eb6ec08a0d0de36e
soomart <alstn1559@naver.com>
2024-09-25 16:32:31 +0900

    header, mypage

---
+++ finalProject/src/main/java/com/example/finalProject/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.finalProject.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+
+//    @Secured("ROLE_USER")
+    @GetMapping(value = "/mypage")
+    public String mypage(){
+        return "mypage";
+    }
+}
