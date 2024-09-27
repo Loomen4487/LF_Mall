99c59b2f44bc37d2db1844d8d3543df63ae76c29
soomart <alstn1559@naver.com>
2024-09-27 16:30:22 +0900

    no message

--- finalProject/src/main/java/com/example/finalProject/config/SecurityConfig.java
+++ finalProject/src/main/java/com/example/finalProject/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.example.finalProject.config;
 
-import com.example.finalProject.security.LoginFailureHandler;
-import com.example.finalProject.security.PrincipalOauthUserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -15,10 +13,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security)throws Exception{
         security.csrf(cs->cs.disable())
-                .formLogin(login->login.loginPage("/login").usernameParameter("id").loginProcessingUrl("/loginOk").defaultSuccessUrl("/").failureHandler(new LoginFailureHandler()))
+                .formLogin(login->login.loginPage("/login").usernameParameter("id").loginProcessingUrl("/loginOk").defaultSuccessUrl("/"))
                 .authorizeHttpRequests(ah->ah.requestMatchers("/user/**").authenticated()
                         .anyRequest().permitAll());
-        //.oauth2Login(oauth->oauth.loginPage("/login").userInfoEndpoint(user->user.userService(principalOauthUserService)));
         return security.build();
     }
 
