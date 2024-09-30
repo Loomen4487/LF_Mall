e51cf0993afafcd0973cc3de3b80f871173f6014
soomart <alstn1559@naver.com>
2024-09-30 16:52:11 +0900

    배송지추가 db반영

--- finalProject/src/main/java/com/example/finalProject/entity/OrderedEntity.java
+++ finalProject/src/main/java/com/example/finalProject/entity/OrderedEntity.java
@@ -3,6 +3,7 @@ package com.example.finalProject.entity;
 import com.example.finalProject.dto.OrderedDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -21,8 +22,23 @@ public class OrderedEntity {
     @CreationTimestamp
     private Date regDate;
     private String location;
+    private String address;
+    private String detailAddress;
+    private String phone;
+
+    @Builder
+    public OrderedEntity(int idx, int product_idx, String location, String address, String detailAddress, String phone) {
+        this.idx = idx;
+        this.product_idx = product_idx;
+        this.regDate = new Date();
+        this.location = location;
+        this.address = address;
+        this.detailAddress = detailAddress;
+        this.phone = phone;
+
+    }
 
     public OrderedDTO toDTO(){
-        return new OrderedDTO(idx,product_idx,regDate,location);
+        return new OrderedDTO(idx,product_idx,regDate,location ,address,detailAddress,phone);
     }
 }
